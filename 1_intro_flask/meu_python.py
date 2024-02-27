1a5b20d5a060ddc8b8f5ed9b57df6055ae936784
carloshernani-CH <carloshcdpg@al.insper.edu.br>
2024-02-27 13:54:39 -0300

    COMMIT

--- 1_intro_flask/meu_python.py
+++ 1_intro_flask/meu_python.py
@@ -1,6 +1,13 @@
 from flask import Flask, request
+import psycopg2 
 
 app = Flask(__name__)
+
+conn = psycopg2.connect(
+    dbname="zevkaiok",
+    user="zevkaiok",
+    password="j2F-SsTiodSnXBpyyQSoCAdoH_cKv9Ao",
+    host="silly.db.elephantsql.com")
 
 lista_alunos = []
 
@@ -11,12 +18,24 @@ def hello_world():
 @app.route("/cadastra_aluno", methods=["POST"])
 def cadastra_aluno_func():
     dic_aluno = request.json
-    nome = dic_aluno.get("nome", "") # dic_aluno["nome"]
-    if nome == "":
-        resp = {"erro": "Nome não informado"}
-        return resp, 400
+    nome = dic_aluno.get("nome", "")
+    idade = dic_aluno.get("idade", 0)
+    cpf = dic_aluno.get("cpf", None)
+    email = dic_aluno.get("email", None)
 
-    lista_alunos.append(dic_aluno)
+    # DE OBRIGATORIO SERÃO OS CAMPOS CPF E EMAIL
+    if not cpf or not email:
+        return {"erro": "CPF e Email são obrigatórios"}, 400
+    
+    try:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO alunos (nome, idade, cpf, email) VALUES (%s, %s, %s, %s)", (nome, idade, cpf, email))
+        conn.commit()
+    except psycopg2.Error as e:
+        conn.rollback()
+        return {"erro": str(e)}, 500
+    finally:
+        cur.close()
 
     # resp de sucesso
     resp = {
@@ -28,10 +47,25 @@ def cadastra_aluno_func():
 
 @app.route("/lista_alunos", methods=["GET"])
 def lista_alunos_func():
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT * FROM alunos")
+        lista_alunos = cur.fetchall()
+    except psycopg2.Error as e:
+        return {"erro": str(e)}, 500
+    finally:
+        cur.close()
 
-    resp = {"alunos": lista_alunos}
-    return resp
-
+    alunos_lista = []
+    for aluno in lista_alunos:
+        alunos_lista.append({
+            "id": aluno[0],
+            "nome": aluno[1],
+            "idade": aluno[4],
+            "cpf": aluno[2],
+            "email": aluno[3]
+        })
+    return alunos_lista, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
